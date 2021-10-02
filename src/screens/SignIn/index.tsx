1ce83fd72a8dca43d5c4bb5f20205d04d5dce43d
Munir Arabi Marques <munirarabi04@gmail.com>
2021-10-02 20:22:01 -0300

    iniciando perfil

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {
-  View,
-  Text,
-  Image,
-  StatusBar
+    View,
+    Text,
+    Image,
+    StatusBar
 } from 'react-native';
 
 import { Buttonicon } from '../../components/ButtonIcon';
@@ -11,39 +11,39 @@ import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
 export function SignIn() {
-  return (
-    <View style={styles.container}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
+    return (
+        <View style={styles.container} >
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
 
-      <Image
-        source={IllustrationImg}
-        style={styles.image}
-        resizeMode="stretch"
-      />
+            <Image
+                source={IllustrationImg}
+                style={styles.image}
+                resizeMode="stretch"
+            />
 
-      <View style={styles.content}>
-        <Text style={styles.title}>
-          Organize {`\n`}
-          suas jogatinas {`\n`}
-          facilmente
-        </Text>
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Organize {`\n`}
+                    suas jogatinas {`\n`}
+                    facilmente
+                </Text>
 
-        <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {`\n`}
-          favoritos com seus amigos
-        </Text>
+                < Text style={styles.subtitle} >
+                    Crie grupos para jogar seus games {`\n`}
+                    favoritos com seus amigos
+                </Text>
 
-        <Buttonicon
-          title="Entrar com Discord"
-          activeOpacity={0.6}
-        />
+                < Buttonicon
+                    title="Entrar com Discord"
+                    activeOpacity={0.6}
+                />
 
 
-      </View>
-    </View>
-  )
+            </View>
+        </View>
+    )
 }
